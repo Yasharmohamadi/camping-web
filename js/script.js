ce96f6566a0c19e8c63d732b52416cbf9a0728be
Yashar <yashar22mo@gmail.com>
2024-06-07 17:03:08 +0330

    Add a script for mobile nav showing up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const menuBtn = document.querySelector('.nav__btn')
+const mobileNav = document.querySelector('.mobile-nav')
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('nav__btn--open')
+
+    let btnClass = [...menuBtn.classList]
+
+    console.log(btnClass);
+    if (btnClass.includes('nav__btn--open')) {
+        mobileNav.style.right = '0'
+    } else {
+        mobileNav.style.right = '-30rem'
+    }
+
 })
