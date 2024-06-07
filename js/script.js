56bc716732db3d01c345e50b69ec198ec5700162
Yashar <yashar22mo@gmail.com>
2024-06-07 20:48:30 +0330

    add active clas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.nav__btn')
 const mobileNav = document.querySelector('.mobile-nav')
+const menuItems = document.querySelectorAll('.menu__item-link')
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('nav__btn--open')
@@ -12,4 +13,10 @@ menuBtn.addEventListener('click', () => {
         mobileNav.style.right = '-30rem'
     }
 
+})
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('menu__item-link--active')
+    })
 })
